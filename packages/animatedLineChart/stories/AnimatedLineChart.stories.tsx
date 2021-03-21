26ac9e0dff71643d720a7914877fea02ba4404a6
MaksimEpifanau <maksim.epifanau@gmail.com>
2021-03-21 20:04:03 +0300

    done with basic AnimatedLineChart

---
+++ packages/animatedLineChart/stories/AnimatedLineChart.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import AnimatedLineChart, { AnimatedLineChartProps } from './AnimatedLineChart';
+import { generateData } from './../../generators/src/animatedLineChartTestData';
+
+export default {
+  title: 'Example/AnimatedLineChart',
+  component: AnimatedLineChart,
+} as Meta;
+
+const COMMON_PROPS = {
+  data: generateData()
+}
+
+const Template: Story<AnimatedLineChartProps> = (args) => <AnimatedLineChart {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  ...COMMON_PROPS,
+};
